57093617575898e4efa4efda7574cadd9b691d2e
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-07-08 23:50:00 +0900

    Update: Made action function

--- src/redux/modules/reduxPoint.js
+++ src/redux/modules/reduxPoint.js
@@ -1,20 +1,70 @@
-const RECORDRATING = "record daily rating";
+export const DAILYRATING = "record daily rating";
+export const LASTWEEK = "record last week";
+export const TWOWEEKSAGO = "record two weeks ago";
 
-const initialState = {
-  point: [],
+export const initialState = {
+  point: [
+    { day: "일", rating: 0 },
+    { day: "월", rating: 0 },
+    { day: "화", rating: 0 },
+    { day: "수", rating: 0 },
+    { day: "목", rating: 0 },
+    { day: "금", rating: 0 },
+    { day: "토", rating: 0 },
+  ],
+  avgRating: Number(0).toFixed(1),
+  lastWeek: Number(0).toFixed(1),
+  twoWeeksAgo: Number(0).toFixed(1),
 };
 
-const reducer = (state = {}, action = {}) => {
+const pointReducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    case RECORDRATING:
-      return console.log("리듀서의 레코딩 케이스 입니다.");
+    case DAILYRATING: {
+      // avgRating도 함께 변경
+      const thisDay = JSON.parse(JSON.stringify(state.point));
+      thisDay.map((el) =>
+        el.day === action.index ? (el.rating = action.rating) : el
+      );
+      const newAvg = makeAvgPoint(thisDay.map((el) => el.rating));
+      return { ...state, point: thisDay, avgRating: newAvg };
+    }
+    case LASTWEEK: {
+      // 지난 주 대비 증감률 적기
+      return console.log("LASTWEEK");
+    }
+    case TWOWEEKSAGO: {
+      // 지난 주 대비 증감률 적기
+      return console.log("TWOWEEKSAGO");
+    }
     default:
       return state;
   }
 };
+// DAILYRATING 액션함수
+export function dailyRatingFunc(index, rating) {
+  return { type: DAILYRATING, index: index.day, rating };
+}
 
-export const recordRatingControler = (widget) => {
-  return console.log("레코딩 액션함수 입니다.");
-};
+// 평균을 구하는 함수
+function makeAvgPoint(point) {
+  const Today = new Date().getDay();
+  const avgs = point;
+  const calcAvgs = (
+    avgs.reduce((acc, el, i) => (i <= Today ? acc + el : acc), 0) /
+    (Today + 1)
+  ).toFixed(1);
+  return calcAvgs;
+}
 
-export default reducer;
+// initialState에서 Point객체를 넘겨주면 오늘 날짜에 맞게 전환시켜줌.
+export function setDates(point) {
+  const DateArray = point;
+  const Today = new Date().getDay();
+  const arrange = [];
+  for (let i = 0; i < 7; i++) {
+    arrange[i] = DateArray[(Today + i) % 7];
+  }
+  return arrange;
+}
+
+export default pointReducer;
