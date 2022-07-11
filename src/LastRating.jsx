c3800f53460f6203eddec895f86d2b2fd7f9bdcf
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-07-11 10:50:37 +0900

    Update: New Component

---
+++ src/LastRating.jsx
@@ -0,0 +1,13 @@
+// 지난 주간의 평점들을 기록
+import React from "react";
+
+const LastWeek = () => {
+  return (
+    <>
+      <div>2주 전 평균 평점</div>
+      <div>지난 주 평균 평점</div>
+    </>
+  );
+};
+
+export default LastWeek;
--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, memo } from "react";
+import React, { memo } from "react";
 import DayBucket from "./DayBucket";
 import { useSelector } from "react-redux";
 import { setDates } from "./redux/modules/reduxPoint";
 import { Avg } from "./styledComponent";
+import LastRating from "./LastRating";
 
 const Home = memo(() => {
   // point객체 가져오기
@@ -12,15 +13,12 @@ const Home = memo(() => {
   // 평균 점수 구하기
   const avg = useSelector((state) => state.reducer.avgRating);
 
-  const ResetBtn = useRef(null); // 리셋 버튼
-
   return (
     <>
       <h1>캠프 일주일 평점</h1>
       <DayBucket Week={Date} />
       <Avg>평균 평점</Avg>
       <Avg>{avg}</Avg>
-      <button ref={ResetBtn}>Reset</button>
     </>
   );
 });
